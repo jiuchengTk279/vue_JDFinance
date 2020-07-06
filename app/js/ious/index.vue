c551698c299f154417c393c4679a789d14574a42
jiucheng <15279232381@163.com>
2020-07-06 22:33:28 +0800

    京东金融白条页完成

---
+++ app/js/ious/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <HeaderTop></HeaderTop>
+    <Slider></Slider>
+    <Welfare></Welfare>
+    <Service></Service>
+    <FooterBottom></FooterBottom>
+    <NavBar></NavBar>
+  </div>
+</template>
+
+<script>
+import HeaderTop from '../public/header.vue'
+import FooterBottom from '../public/footer.vue'
+import NavBar from '../public/navbar.vue'
+import Slider from './lslider.vue'
+import Welfare from './welfare.vue'
+import Service from './service.vue'
+
+export default {
+  components: {
+    HeaderTop,
+    FooterBottom,
+    NavBar,
+    Slider,
+    Welfare,
+    Service
+  }
+}
+</script>
+
+<style lang="scss" scoped module>
+
+</style>
